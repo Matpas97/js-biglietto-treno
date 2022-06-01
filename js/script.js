6b661cd3ae35d41279f0c07c0c8b0ef21358b192
Matpas97 <passarellim22@gmail.com>
2022-06-01 15:21:19 +0200

    js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana 
+
+// Chiedo all'utente il numeroi di km che vuole percorrere
+
+let userKm = parseInt( prompt('Gentile utente, quanti km vuole percorrere ?') );
+
+// Chiedo all'utente la sua eta
+let userAge = parseInt(prompt("Gentile utente, qual'è la sua eta ?") );
+
+// Calcolo sconto del 20% per i minorenni
+const userMinor = ( 0.21 - (0.21 * 20 / 100));
+
+// Calcolo sconto del 40% per gli over 65 
+const userAdult = ( 0.21 - (0.21 * 40 / 100));
+
+// Calcolo prezzo totale del viaggio per i minorenni
+
+let totalTrip;
+
+if (userMinor > 18 )
